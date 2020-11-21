9fe2559839303749fe3ac76d0e600ff140fa529e
AshRaven521 <56455248+AshRaven521@users.noreply.github.com>
2020-11-21 19:58:24 +0300

    Lr3(raw, but for checks)

---
+++ 3 semestr/C#/Lr3/Lr3WpfApplication/ConnectionWithWindowService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Windows;
+namespace Lr2WpfApplication
+{
+    class ConnectionWithWindowService
+    {
+        private static string service = "Lr2 Windows Service";
+
+        private static ServiceController controller = new ServiceController(service);
+
+        public void StartService()
+        {
+            if (controller.Status != ServiceControllerStatus.Running)
+            {
+                controller.Start();
+                controller.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
+                MessageBox.Show("was succesfully launched");
+            }
+            else
+            {
+                MessageBox.Show("is already running");
+            }
+        }
+
+        public void StopService()
+        {
+            if (controller.Status != ServiceControllerStatus.Stopped)
+            {
+                controller.Stop();
+                controller.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
+                MessageBox.Show("was successfully stopped");
+            }
+            else
+            {
+                MessageBox.Show("is already stopped");
+            }
+        }
+
+        private void ShowMessage(string eventDescription, string serviceEvent)
+        {
+            MessageBox.Show($"The service {eventDescription}", $"{serviceEvent}");
+            
+        }
+    }
+}
---
+++ 3 semestr/C#/Lr3/Lr3WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Lr2WpfApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConnectionWithWindowService connect = new ConnectionWithWindowService();
+      
+        public MainWindow()
+        {
+            InitializeComponent();
+            sourceText.Text = @"C:\PTUIR\3 semestr\ISP\Lr2\source";
+            archiveText.Text = @"C:\PTUIR\3 semestr\ISP\Lr2\archive";
+            extractText.Text = @"C:\PTUIR\3 semestr\ISP\Lr2\extract";
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connect.StopService();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderWatcher sourceWatcher = new FolderWatcher(sourceText.Text);
+            FolderWatcher archiveWatcher = new FolderWatcher(archiveText.Text);
+            FolderWatcher extractWatcher = new FolderWatcher(extractText.Text);
+            sourceWatcher.StartWatching();
+            archiveWatcher.StartWatching();
+            extractWatcher.StartWatching();
+            connect.StartService();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            connect.StopService();
+        }
+    }
+}
