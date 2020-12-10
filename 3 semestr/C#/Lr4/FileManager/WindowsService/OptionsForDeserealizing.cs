076c301280b407b54290e2ac649eb45b2398e7e7
AshRaven521 <ivan.kondrashov.92@mail.ru>
2020-12-10 17:33:23 +0300

    Lr4
    Lr4 and videofiles with functional are added

---
+++ 3 semestr/C#/Lr4/FileManager/WindowsService/OptionsForDeserealizing.cs
@@ -0,0 +1,42 @@
+﻿namespace Lr2WindowsService
+{
+    public class OptionsForDeserealizing
+    {
+        public string TargetDirectoryPath { get; set; }
+
+        public string SourceDirectoryPath { get; set; }
+
+        public string ExtractDirectoryPath { get; set; }
+
+        public string BeginFolder { get; set; }
+
+        public string EndFolderForXml { get; set; }
+
+        public string EndFolderForJson { get; set; }
+
+        public string connectionString { get; set; }
+
+        public string connectionStringToErrors { get; set; }
+
+        public OptionsForDeserealizing()
+        {
+            TargetDirectoryPath = @"C:\PTUIR\3 semestr\ISP\Lr3\archive";
+
+            SourceDirectoryPath = @"C:\PTUIR\3 semestr\ISP\Lr3\source";
+
+            ExtractDirectoryPath = @"C:\PTUIR\3 semestr\ISP\Lr3\extract";
+
+            BeginFolder = @"C:\PTUIR\3 semestr\ISP\Lr4";
+
+            EndFolderForXml = @"C:\PTUIR\3 semestr\ISP\Lr3\source\config2.xml";
+
+            EndFolderForJson = @"C:\PTUIR\3 semestr\ISP\Lr3\source\appsettings2.json";
+
+            connectionString = "Server=LAPTOP-VAJ5IJTR;Database=AdventureWorks2019;Trusted_Connection=True;Connect Timeout = 30";
+
+            connectionStringToErrors = "Server=LAPTOP-VAJ5IJTR;Database=ErrorLogs;Trusted_Connection=True";
+        }
+
+    }
+
+}
---
+++ 3 semestr/C#/Lr4/ServiceLayer/XMLFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Models;
+using System.IO;
+using DataAccessLayer;
+
+namespace ServiceLayer
+{
+    public class XMLFile : IConfigGenerating
+    {
+        private string xmlPath = @"C:\PTUIR\3 semestr\ISP\Lr4\config2.xml";
+        private List<Person> person = new List<Person>();
+        private ConnectionToDataBase connect = new ConnectionToDataBase();
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Person>));
+
+        public XMLFile()
+        {
+            person = connect.ConnectAndInteract();
+        }
+        public void Generate() 
+        {
+            using (var xmlStream = new FileStream(xmlPath, FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(xmlStream, person);
+            }
+        }
+            
+    }
+}
